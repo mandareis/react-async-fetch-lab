04333631fae85ba1cc50df73341497e03bf52bc3
mandareis <amanda@depaula.xyz>
2020-12-15 14:15:36 -0800

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,19 @@
 // create your App component here
+import React from "react";
+
+class App extends React.Component {
+  //   constructor() {
+  //     super();
+  //     this.state = { data: [] };
+  //   }
+  async componentDidMount() {
+    const response = await fetch("http://api.open-notify.org/astros.json");
+    const json = await response.json();
+    // this.setState({ data: json });
+  }
+
+  render() {
+    return;
+  }
+}
+export default App;
